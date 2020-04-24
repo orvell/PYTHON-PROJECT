3391d99f6e0ff18c85a2da5c21d26abb9a5ef778
ORVELL FERREIRA <56030574+orvell@users.noreply.github.com>
2020-04-24 11:59:28 +0530

    Add files via upload

--- project/ProjFinal.py
+++ project/ProjFinal.py
@@ -9,8 +9,8 @@ heading.config(font=("Helvetica", 19,"bold"))
 
 
 photo = PhotoImage(file="doc.png")
-label = Label(root, image=photo)
-label.place(x=170, y=100)
+label = Label(root, image=photo )
+label.place(x=120, y=70)
 
 toolbar = Frame(root, bg="blue2")
 toolbar.place(x=0, y=450, height=55, width=800)
@@ -24,7 +24,7 @@ def open_entry():
     testno = StringVar()
     name = StringVar()
     age = StringVar()
-    hospital = StringVar()
+    mobno = StringVar()
     address = StringVar()
     day = StringVar()
     month = StringVar()
@@ -33,7 +33,8 @@ def open_entry():
     l1 = Label(top, text="Testno.  :", bg="light cyan",font=("Helvetica", 9,"bold"))
     l2 = Label(top, text="Name  :", bg="light cyan",font=("Helvetica", 9,"bold"))
     l3 = Label(top, text="Age  :", bg="light cyan",font=("Helvetica", 9,"bold"))
-    l4 = Label(top, text="Hospital  :", bg="light cyan",font=("Helvetica", 9,"bold"))
+    l4 = Label(top, text="Mobile No  :", bg="light cyan",font=("Helvetica", 9,"bold"))
+    l9 = Label(top, text="(Mobile number should be 10 digit and start with either 7, 8, 9)", bg="light cyan",font=("Helvetica", 8,"bold"))
     l5 = Label(top, text="Address  :", bg="light cyan",font=("Helvetica", 9,"bold"))
     l6 = Label(top, text="Date  :", bg="light cyan",font=("Helvetica", 9,"bold"))
     l7 = Label(top, text="DD  MM  YYYY", bg="light cyan",font=("Helvetica", 9,"bold"))
@@ -42,7 +43,8 @@ def open_entry():
     l1.place(x=170, y=100, height=25, width=60)
     l2.place(x=170, y=200, height=25, width=60)
     l3.place(x=170, y=300, height=25, width=60)
-    l4.place(x=170, y=400, height=25, width=60)
+    l4.place(x=170, y=400, height=25, width=65)
+    l9.place(x=130, y=430, height=20, width=400)
     l5.place(x=420, y=100, height=25, width=60)
     l6.place(x=420, y=200, height=25, width=60)
     l7.place(x=500, y=230, height=20, width=100)
@@ -51,7 +53,7 @@ def open_entry():
     e1 = Entry(top, textvariable=testno).place(x=250, y=103)
     e2 = Entry(top, textvariable=name).place(x=250, y=203)
     e3 = Entry(top, textvariable=age).place(x=250, y=303)
-    e4 = Entry(top, textvariable=hospital).place(x=250, y=403)
+    e4 = Entry(top, textvariable=mobno).place(x=250, y=403)
     e5 = Entry(top, textvariable=address).place(x=500, y=103)
     e6 = Entry(top, textvariable=day).place(x=500, y=203, width=20)
     e7 = Entry(top, textvariable=month).place(x=530, y=203, width=20)
@@ -66,24 +68,66 @@ def open_entry():
         d = year.get()
         d = d + "-" + month.get() + "-" + day.get()
         e = age.get()
-        f = hospital.get()
-        if (a!="" and b!="" and c!="" and d!="" and e!="" and f!=""):
-            sql = "INSERT INTO patients (Testno ,Address ,Nameofpatient,Datefound,Age ,Hospital) VALUES (%s, %s,%s, %s,%s, %s)"
-            val = (a, b, c, d, e, f)
-            mycursor.execute(sql, val)
-            mydb.commit()
-            mydb.close()
-            messagebox.showinfo('Success','Registration Succussful')
-            testno.set("")
-            address.set("")
-            name.set("")
-            year.set("")
-            month.set("")
-            day.set("")
-            age.set("")
-            hospital.set("")
+        f= mobno.get()
+        x=""
+        if f!="":
+            len_tmp_str=len(f)
+            if(len_tmp_str!=10):
+                 ##"LENGTH PROBLEM"
+                x="Invalid"
+                messagebox.showerror('Error','Error! Invalid Mobile Number')
+            elif(f[0]!="7" and f[0]!="8" and f[0]!="9"):
+            ##"START PROBLEM"
+                 x="Invalid"
+                 messagebox.showerror('Error','Error! Invalid Mobile Number')
+            else:
+                check=1
+                for i in f:
+                    if(i>="0" and i<="9"):
+                        continue
+                    else:
+                        check=0
+                        break
+                if(check==1):
+                   f= mobno.get()
+                else:
+                    ##"NUMBER PROBLEM"
+                     x="Invalid"
+                     messagebox.showerror('Error','Error! Invalid Mobile Number')
+       
+        mycursor.execute("SELECT Testno FROM patients")
+        check=mycursor.fetchall()
+        nos=list(sum(check,()))
+        N=a
+        if N=="":
+           messagebox.showerror('Error','Error! You have left some fields empty')
         else:
-            messagebox.showerror('Error','Error! You have left some fields empty')
+            N=int(N)
+        
+        if N in nos:
+            messagebox.showerror('Error','Error! The Test No already Exist, Please enter a unique number')
+        else:
+            if x=="Invalid":
+                pass
+            else:
+                if (a!="" and b!="" and c!="" and d!="" and e!="" and f!=""):
+                    sql = "INSERT INTO patients (Testno ,Address ,Nameofpatient,Datefound,Age ,Mobno) VALUES (%s, %s,%s, %s,%s, %s)"
+                    val = (a, b, c, d, e, f)
+                    mycursor.execute(sql, val)
+                    mydb.commit()
+                    mydb.close()
+                    messagebox.showinfo('Success','Registration Succussful')
+                    testno.set("")
+                    address.set("")
+                    name.set("")
+                    year.set("")
+                    month.set("")
+                    day.set("")
+                    age.set("")
+                    mobno.set("")
+                else:
+                    messagebox.showerror('Error','Error! You have left some fields empty')
+            
 
     sub_butt = Button(top, text="Submit", bg="blue2", command=lambda:[submit(),cases()],fg="Lightgrey",font=("Helvetica", 10,"bold"),bd=5,relief="raised")
     sub_butt.place(x=420, y=300, height=25, width=60)
@@ -105,7 +149,7 @@ def open_edit():
     no = StringVar()
     name = StringVar()
     age = StringVar()
-    hospital = StringVar()
+    mobno = StringVar()
     address = StringVar()
     day = StringVar()
     month = StringVar()
@@ -117,7 +161,10 @@ def open_edit():
         check=mycursor.fetchall()
         nos=list(sum(check,()))
         N=no.get()
-        N=int(N)
+        if N=="":
+           messagebox.showerror('Error','Error! You have left some fields empty')
+        else:
+            N=int(N)
         mydb.close()
         if N in nos:
             top1 = Toplevel()
@@ -126,21 +173,23 @@ def open_edit():
 
             l1 = Label(top1, text="Name  :", bg="light cyan",font=("Helvetica", 9,"bold"))
             l2 = Label(top1, text="Age  :",  bg="light cyan",font=("Helvetica", 9,"bold"))
-            l3 = Label(top1, text="Hospital  :",  bg="light cyan",font=("Helvetica", 9,"bold"))
+            l3 = Label(top1, text="Mobile No  :",  bg="light cyan",font=("Helvetica", 9,"bold"))
+            l9 = Label(top1, text="(Mobile number should be 10 digit and start with either 7, 8, 9)", bg="light cyan",font=("Helvetica", 8,"bold"))
             l5 = Label(top1, text="Address  :",  bg="light cyan",font=("Helvetica", 9,"bold"))
             l6 = Label(top1, text="Date  :", bg="light cyan",font=("Helvetica", 9,"bold"))
             l7 = Label(top1, text="DD    MM    YYYY", bg="light cyan",font=("Helvetica", 9,"bold"))
 
             l1.place(x=170, y=100, height=25, width=60)
             l2.place(x=170, y=200, height=25, width=60)
-            l3.place(x=170, y=300, height=25, width=60)
+            l3.place(x=170, y=300, height=25, width=65)
             l5.place(x=420, y=100, height=25, width=60)
             l6.place(x=420, y=200, height=25, width=60)
             l7.place(x=490, y=230, height=20, width=120)
+            l9.place(x=130, y=330, height=20, width=400)
 
             e1 = Entry(top1,textvariable=name).place(x=250, y=103) 
             e2 = Entry(top1,textvariable=age).place(x=250, y=203) 
-            e3 = Entry(top1,textvariable=hospital).place(x=250, y=303) 
+            e3 = Entry(top1,textvariable=mobno).place(x=250, y=303) 
             e5 = Entry(top1,textvariable=address).place(x=500, y=103) 
             e6 = Entry(top1,textvariable=day).place(x=500, y=203, width=20) 
             e7 = Entry(top1,textvariable=month).place(x=530, y=203, width=20) 
@@ -153,42 +202,70 @@ def open_edit():
                 d = year.get()
                 d = d + "-" + month.get() + "-" + day.get()
                 e = age.get()
-                f = hospital.get()
-                
-                
-                if (b!="" and c!="" and d!="" and e!="" and f!=""):
-                    sql = "UPDATE patients SET Address=%s WHERE Testno=%s"
-                    val = (b,N)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                    sql = "UPDATE patients SET Nameofpatient=%s WHERE Testno=%s"
-                    val = (c,N)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                    sql = "UPDATE patients SET Datefound=%s WHERE Testno=%s"
-                    val = (d,N)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                    sql = "UPDATE patients SET Age=%s WHERE Testno=%s"
-                    val = (e,N)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                    sql = "UPDATE patients SET Hospital=%s WHERE Testno=%s"
-                    val = (f,N)
-                    mycursor.execute(sql, val)
-                    mydb.commit()
-                    mydb.close()
-                    messagebox.showinfo('Success','Updated Succussfully')
-                    
-                    address.set("")
-                    name.set("")
-                    year.set("")
-                    month.set("")
-                    day.set("")
-                    age.set("")
-                    hospital.set("")
+                f = mobno.get()
+                x=""
+                if f=="":
+                    pass
                 else:
-                    messagebox.showerror('Error','Error! You have left some fields empty')
+                    len_tmp_str=len(f)
+                    if(len_tmp_str!=10):
+                    ##"LENGTH PROBLEM"
+                        x="Invalid"
+                        messagebox.showerror('Error','Error! Invalid Mobile Number')
+                    elif(f[0]!="7" and f[0]!="8" and f[0]!="9"):
+                    ##"START PROBLEM"
+                        x="Invalid"
+                        messagebox.showerror('Error','Error! Invalid Mobile Number')
+                    else:
+                        check=1
+                        for i in f:
+                            if(i>="0" and i<="9"):
+                                continue
+                            else:
+                                check=0
+                                break
+                        if(check==1):
+                            pass
+                        else:
+                        ##"NUMBER PROBLEM"
+                            x="Invalid"
+                            messagebox.showerror('Error','Error! Invalid Mobile Number')
+                if x=="Invalid":
+                    pass
+                else:
+                    if (b!="" and c!="" and d!="" and e!="" and f!=""):
+                        sql = "UPDATE patients SET Address=%s WHERE Testno=%s"
+                        val = (b,N)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+                        sql = "UPDATE patients SET Nameofpatient=%s WHERE Testno=%s"
+                        val = (c,N)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+                        sql = "UPDATE patients SET Datefound=%s WHERE Testno=%s"
+                        val = (d,N)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+                        sql = "UPDATE patients SET Age=%s WHERE Testno=%s"
+                        val = (e,N)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+                        sql = "UPDATE patients SET Mobno=%s WHERE Testno=%s"
+                        val = (f,N)
+                        mycursor.execute(sql, val)
+                        mydb.commit()
+                        mydb.close()
+                        messagebox.showinfo('Success','Updated Succussfully')
+                    
+                        address.set("")
+                        name.set("")
+                        year.set("")
+                        month.set("")
+                        day.set("")
+                        age.set("")
+                        mobno.set("")
+                    else:
+                        messagebox.showerror('Error','Error! You have left some fields empty')
                 
             sub_butt = Button(top1, text="Submit", bg="Blue2",command=submit,fg="Lightgrey",font=("Helvetica", 10,"bold"),bd=5,relief="raised")
             sub_butt.place(x=420, y=300, height=25, width=60)
@@ -248,7 +325,10 @@ def open_delete():
         check=mycursor.fetchall()
         nos=list(sum(check,()))
         N=no.get()
-        N=int(N)
+        if N=="":
+           messagebox.showerror('Error','Error! You have left some fields empty')
+        else:
+            N=int(N)
         mydb.close()
         if N in nos:
             mydb = mysql.connector.connect(host="localhost", user="root", passwd="orvell1830", database="db1") # change password here
@@ -288,7 +368,7 @@ def open_view():
     top.resizable(width=False, height=False)
 
     top.configure(bg="light cyan")
-    l1 = Label(upper, text="Test No                              Name of patient                             Address                            Age                          Hospital                 Date ", bg="blue2",fg="lightgrey",font=("Helvetica", 8,"bold"))
+    l1 = Label(upper, text="Test No                              Name of patient                             Address                            Age                          Mobile No                 Date ", bg="blue2",fg="lightgrey",font=("Helvetica", 8,"bold"))
     l1.pack(fill=X)
     mydb = mysql.connector.connect(host="localhost", user="root", passwd="orvell1830", database="db1") # change password here
     mycursor = mydb.cursor()
@@ -300,7 +380,7 @@ def open_view():
     query_label4=Label(top,text=print_records,width=8,bg="light cyan")
     query_label4.place(x=683,y=25)
 
-    mycursor.execute("SELECT Hospital FROM patients")
+    mycursor.execute("SELECT Mobno FROM patients")
     records=mycursor.fetchall()
     print_records= ''
     for record in records:
@@ -356,7 +436,7 @@ def open_help():
     l5 = Label(top, text=" create database db1", bg="light cyan",anchor=W)
     l6 = Label(top, text=" use db1", bg="light cyan",anchor=W)
     l7 = Label(top, text=" create table patients(Testno int unique,Address text NOT NULL,Nameofpatient text NOT NULL,Datefound Date NOT NULL", bg="light cyan",anchor=W)
-    l10 = Label(top, text=" ,Age int NOT NULL,Hospital text NOT NULL)", bg="light cyan",anchor=W)
+    l10 = Label(top, text=" ,Age int NOT NULL,Mobno numeric(10))", bg="light cyan",anchor=W)
     l8 = Label(top, text="4.Please change the password in the code to your root password.", bg="light cyan",anchor=W)
     l11 = Label(top, text="5.Run the following command in CMD(Windows) (pip install mysql-connector-python).", bg="light cyan",anchor=W)
     l9 = Label(top, text="6.For any further assistance please contact us at abc@fake.com", bg="light cyan",anchor=W)
@@ -414,8 +494,8 @@ butt5 = Button(root, text="Help", command=open_help, bg="DodgerBlue2",bd=5,relie
 butt5.place(x=638, y=461, height=30, width=65)
 
 cas = Label(root, text="Total People Infected:",bg="light cyan",anchor=E)
-cas.place(x=270, y=53, height=25, width=200)
-cas.config(font=("Helvetica", 10,"bold"))
+cas.place(x=270, y=40, height=25, width=200)
+cas.config(font=("Helvetica", 12,"bold"))
 
 
 
@@ -428,9 +508,9 @@ def cases():
     check=mycursor.fetchall()
     nos=list(sum(check,()))
     x=len(nos)
-    lab=Label(root,text=x,bg="light cyan")
-    lab.place(x=470,y=53, height=25)
-    lab.config(font=("Helvetica", 10,"bold"))
+    lab=Label(root,text=x,bg="light cyan",fg="red")
+    lab.place(x=480,y=40, height=25)
+    lab.config(font=("Helvetica", 16,"bold"))
 
 
 cases()
@@ -439,4 +519,3 @@ root.configure(bg="light cyan")
 root.geometry("800x500+200+100")
 root.resizable(width=False, height=False)
 root.mainloop()
-
